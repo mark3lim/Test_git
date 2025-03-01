0d4826e0026b3e511d9ee4ad0c4853b0326ac982
taegyunlim <xorbs228@gmail.com>
2025-03-01 16:41:49 +0900

    Revert "머지2?"
    This reverts commit 9b83b57ac86fd3df586b5e12b48e4ae199f25670.

--- test_git/src/kr/co/sist/test/Ltg.java
+++ test_git/src/kr/co/sist/test/Ltg.java
@@ -4,8 +4,6 @@ public class Ltg {
 
 	public static void main(String[] args) {
 		System.out.println("이것은 신기능 추가!!!!!!");
-		System.out.println("대포동 미사일");
-		System.out.println("임태균의 미소!! ^_^");
 	}
 
 }
